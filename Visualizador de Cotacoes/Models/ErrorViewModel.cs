aaf8e080cfd08f5b51652f387ad7fadb6dab7168
Ryan <ryanmedeiros1000@gmail.com>
2025-06-17 10:52:08 -0300

    Initial Project

---
+++ Visualizador de Cotacoes/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Visualizador_de_Cotacoes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

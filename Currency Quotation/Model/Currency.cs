e3a9bffe3fe86d1fca15cbb3db8bd2de7f5a9541
Ryan <ryanmedeiros1000@gmail.com>
2025-06-18 17:24:08 -0300

    Adding the class 'Currency'

---
+++ Currency Quotation/Model/Currency.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Text.Json.Serialization;
+
+namespace Currency_Quotation.Model;
+
+public class Currency
+{
+	[JsonPropertyName("moeda")]
+	public string Coin { get; set; }
+	[JsonPropertyName("nome")]
+	public string Name { get; set; }
+	[JsonPropertyName("compra")]
+	public decimal BuyPrice { get; set; }
+	[JsonPropertyName("venda")]
+	public decimal SellPrice { get; set; }
+	[JsonPropertyName("fechoAnterior")]
+	public decimal PreviousClosure { get; set; }
+	[JsonPropertyName("dataAtualizacao")]
+	public DateTime UpdateDate { get; set; }
+}
